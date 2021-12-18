0f8dd2c4a2f5b0035c53c43977576a0a0f6d2222
Aleksandr-Pod <stdfire@gmail.com>
2021-12-18 19:22:21 +0200

    mod-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(SimpleLightbox);
+
+const gallery = galleryItems.map(({ preview, original, description }) => {
+    return `<li>
+        <a class="gallery__item" href=${original}>
+            <img
+                class="gallery__image"
+                src=${preview}
+                alt=${description}
+            />
+        </a>
+    </li>`
+}).join("");
+
+const galleryRef = document.querySelector(".gallery");
+galleryRef.insertAdjacentHTML("afterbegin", gallery);
+
+const modal = new SimpleLightbox(".gallery__item");
+
+modal.options.captionsData = "alt";
+modal.options.captionDelay = 250;
+modal.on('show.simplelightbox');
